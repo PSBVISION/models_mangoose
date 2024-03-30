2a4b355834d74a793e130c5cf591a6f6a83a1f7c
PSBVISION <punitsinghbisht1529@gmail.com>
2024-03-30 22:45:26 +0530

    updates in todo model

--- models/todos/sub_todo.models.js
+++ models/todos/sub_todo.models.js
@@ -1,5 +1,19 @@
-import mongoose from "mongoose"
+import mongoose, { trusted } from 'mongoose';
 
-const userSchema = new mongoose.Schema({})
+const subtodoSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true
+    },
+    complete:{
+      type: Boolean,
+      default: false
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    }
+  }, { timestamps: true });
 
-export const User = mongoose.model("User", userSchema)
+export const Subtodo = mongoose.model("Subtodo", subtodoSchema);
