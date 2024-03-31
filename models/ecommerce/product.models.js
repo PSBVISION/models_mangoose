5823660db07de47a0896a06b962dbd743db925c9
PSBVISION <punitsinghbisht1529@gmail.com>
2024-03-31 12:56:59 +0530

    updates in product model

--- models/ecommerce/product.models.js
+++ models/ecommerce/product.models.js
@@ -1,6 +1,27 @@
 import mongoose from 'mongoose';
 
 const productSchema = new mongoose.Schema(
-  {}, {timestamps: true,});
+  {
+    description: {
+      required: true,
+      type: String,
+    },
+    name: {
+      required: true,
+      type: String
+    },
+    productImage:{
+      required: true,
+      type: String
+    },
+    price:{
+      type: Number,
+      default: 0
+    },
+    stock:{
+      type: Number,
+      default: 0
+    },
+  }, {timestamps: true,});
 
 export const Product = mongoose.model("Product", productSchema);
