c0663ac447282e6574ca50b96a223acae7a0f808
PSBVISION <punitsinghbisht1529@gmail.com>
2024-03-31 15:38:01 +0530

    updates in order model

--- models/ecommerce/order.models.js
+++ models/ecommerce/order.models.js
@@ -1,6 +1,38 @@
 import mongoose from "mongoose";
 
+const orderItemsSchema = new mongoose.Schema({
+    productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product"
+    },
+    quantity: {
+        type: Number,
+        required: true,
+    }
+})
+
 const orderSchema = new mongoose.Schema(
-    {},{ timestamps : true });
+    {
+        orderPrice: {
+            type: Number,
+            required: true
+        },
+        customer: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        orderItems: {
+            type: [orderItemsSchema]
+        },
+        address: {
+            type: String,
+            required: true,
+        },
+        status: {
+            type: String,
+            enum: ["PENDING", "CANCELLED", "DELIVERED"],
+            default: "PENDING"
+        }
+    },{ timestamps : true });
 
 export const Order = mongoose.model("Order", orderSchema);
